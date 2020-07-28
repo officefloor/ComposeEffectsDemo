cfae8dda47e507a8d0c96944023e3f9c8e3e8372
Daniel Sagenschneider <daniel@officefloor.net>
2020-07-29 01:50:31 +0800

    Fixing build

--- src/main/java/net/officefloor/demo/SpringSetup.java
+++ src/main/java/net/officefloor/demo/SpringSetup.java
@@ -1,7 +1,22 @@
 package net.officefloor.demo;
 
+import org.h2.Driver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.annotation.PostConstruct;
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
 
 /**
  * Setup for Spring Boot.
@@ -11,4 +26,36 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class SpringSetup {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SpringSetup.class, "--debug");
+    }
+
+    private @Autowired Environment environment;
+
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(Driver.class.getName());
+        dataSource.setUrl(this.environment.getProperty("spring.datasource.url"));
+        dataSource.setUsername(this.environment.getProperty("spring.datasource.username"));
+        dataSource.setPassword(this.environment.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+
+    @Bean
+    public EntityManagerFactory entityManagerFactory() {
+        HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        vendorAdapter.setGenerateDdl(Boolean.FALSE);
+        vendorAdapter.setShowSql(Boolean.TRUE);
+        LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+        factory.setJpaVendorAdapter(vendorAdapter);
+        factory.setDataSource(dataSource());
+        factory.setPackagesToScan("net.officefloor.demo");
+        factory.afterPropertiesSet();
+        return factory.getObject();
+    }
+
+
+
 }
